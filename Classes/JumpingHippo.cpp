4d5a2f4ab529c94dfe2d6278cd3c35767c8da4a3
Hugo <hugo_val28@hotmail.com>
2017-10-14 12:44:14 -0600

    unlimited enemies

--- Classes/JumpingHippo.cpp
+++ Classes/JumpingHippo.cpp
@@ -49,7 +49,7 @@ void JumpingHippo::initSpawner()
         auto moveToLeft = MoveTo::create(2.f, Vec2(0, size.height/2));
         spikesSprite->runAction(moveToLeft);
         
-    }, 1.f, 10, 1.f, "spawnerCallBack");
+    }, 1.f, CC_REPEAT_FOREVER, 1.f, "spawnerCallBack");
 }
 
 void JumpingHippo::initClickListener()
@@ -57,18 +57,16 @@ void JumpingHippo::initClickListener()
     auto mouseListener = EventListenerTouchOneByOne::create();
     
     mouseListener->onTouchBegan = [=](Touch* touch, Event* event){
+        //reinicia la rotacion del hippo en cada click
         hippoSprite ->setRotation(0.0f);
 
         auto jump = JumpBy::create(0.8, Vec2::ZERO, 150, 1);
+        //rotar 360 grados
         auto rotateBy = RotateBy::create(0.8f, 360.0f);
-//        hippoSprite->runAction(jump);
-//        hippoSprite->runAction(rotateBy);
-        
+
         // running the above Actions with Spawn.
         auto mySpawn = Spawn::createWithTwoActions(jump, rotateBy);
         hippoSprite->runAction(mySpawn);
-        
-        
         return true;
     };
     mouseListener->onTouchMoved = [=](Touch* touch, Event* event){};
